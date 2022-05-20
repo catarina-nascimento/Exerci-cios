4e40641c177fd1cf276f7efe340db036d3810b61
CatarinaNascimento <cat.arina.nascimento@outlook.com>
2022-05-20 16:17:47 +0100

    js

---
+++ javascript/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if(vel > 60){ //Condição simples, se true ele aparecer
+    console.log(`Multado`)
+}
+
+console.log(`Use cinto de segurança`)
